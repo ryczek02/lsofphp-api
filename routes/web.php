a8c3da54012a5656db0ea0c4fe37721739aba821
Łukasz Ryczko <lukasz.ryczko@fingoweb.com>
2024-01-06 22:36:45 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function (){
+    return [
+        'PHP_VERSION' => phpversion(),
+        'SERVER' => 'http://' . $_SERVER['HTTP_HOST'] . '/server',
+        'REQUEST' => 'http://' . $_SERVER['HTTP_HOST'] . '/request',
+        'OCTANE' => isset($_SERVER['OCTANE_SERVER']),
+    ];
+});
+
+
+Route::get('/request', function(){
+    return $_REQUEST;
+});
+
+Route::get('/server', function(){
+    return $_SERVER;
+});
+
+Route::get('/php', function(){
+    return ["PHP_VERSION" => phpversion()]  ;
+});
